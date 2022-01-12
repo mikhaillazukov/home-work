4756af63ece0362958f49bd5e27108c46598c376
Mikhail Lazukov <misha130200@gmail.com>
2022-01-12 11:50:37 +0300

    Добавлена сигнатура метода updateContractNumberByClientId в AccountRepository

--- src/main/java/com/sbrf/reboot/repository/AccountRepository.java
+++ src/main/java/com/sbrf/reboot/repository/AccountRepository.java
@@ -1,10 +1,13 @@
 package com.sbrf.reboot.repository;
 
+import java.io.FileNotFoundException;
 import java.math.BigDecimal;
 import java.util.Set;
 
 public interface AccountRepository {
     Set<Long> getAllAccountsByClientId(long clientId);
+
+    void updateContractNumberByClientId(long clientId, long oldContractNumber, long newContractNumber);
 
     BigDecimal getAccountBalanceByContractNumber(long contractNumber);
 }
