522795938c9cd2675c42efe93d49c251672cc346
Mikhail Lazukov <misha130200@gmail.com>
2022-01-12 11:48:38 +0300

    Добавлены тесты для нового метода updateContractNumberByClientId из FileAccountRepository

--- src/test/java/com/sbrf/reboot/repository/FileAccountRepositoryTest.java
+++ src/test/java/com/sbrf/reboot/repository/FileAccountRepositoryTest.java
@@ -3,7 +3,6 @@ package com.sbrf.reboot.repository;
 import com.sbrf.reboot.repository.impl.FileAccountRepository;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
@@ -40,8 +39,40 @@ class FileAccountRepositoryTest {
 
         accountRepository = new FileAccountRepository(filePath);
 
-        assertThrows(FileNotFoundException.class, () -> accountRepository.getAllAccountsByClientId(clientId));
+        assertThrows(RuntimeException.class, () -> accountRepository.getAllAccountsByClientId(clientId));
     }
 
+
+    @Test
+    void updatePersonalAccount() throws IOException {
+        String filePath = "src/main/resources/Accounts.txt";
+        accountRepository = new FileAccountRepository(filePath);
+
+        long clientId = 1L;
+        long oldContractNumber = 111L;
+        long newContractNumber = 123L;
+
+
+        accountRepository.updateContractNumberByClientId(clientId, oldContractNumber, newContractNumber);
+        Set<Long> actualAccounts = accountRepository.getAllAccountsByClientId(clientId);
+
+        assertTrue(!actualAccounts.contains(oldContractNumber) && actualAccounts.contains(newContractNumber));
+
+        accountRepository.updateContractNumberByClientId(clientId, newContractNumber, oldContractNumber);
+    }
+
+    @Test
+    void failUpdatePersonalAccount() {
+        long clientId = 1L;
+        long oldContractNumber = 111L;
+        long newContractNumber = 123L;
+
+        String filePath = "somePath";
+
+        accountRepository = new FileAccountRepository(filePath);
+
+        assertThrows(RuntimeException.class,
+                () -> accountRepository.updateContractNumberByClientId(clientId, oldContractNumber, newContractNumber));
+    }
 
 }
