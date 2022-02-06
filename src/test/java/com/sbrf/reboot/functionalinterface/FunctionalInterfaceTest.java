ab58a76ac6d7daf50e25a5e88b8aff5e940cdf79
Mikhail Lazukov <misha130200@gmail.com>
2022-02-06 14:35:51 +0700

    Дополнена реализация исходного функционального интерфейса и создан собственный пример использования функционального интерфейса.

--- src/test/java/com/sbrf/reboot/functionalinterface/FunctionalInterfaceTest.java
+++ src/test/java/com/sbrf/reboot/functionalinterface/FunctionalInterfaceTest.java
@@ -7,7 +7,10 @@ import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import com.sbrf.reboot.utils.JSONUtils;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -27,24 +30,24 @@ public class FunctionalInterfaceTest {
     }
 
     static class ListConverter<T> {
-        public List<String> toJsonsList(@NonNull List<T> someObjects, ObjectToJsonFunction<T> objectToJsonFunction) {
+        public List<String> toJsonsList(@NonNull List<T> someObjects, ObjectToJsonFunction<T> objectToJsonFunction) throws JsonProcessingException {
             List<String> result = new ArrayList<>();
             if (someObjects.isEmpty())
                 throw new IllegalArgumentException("The list is empty");
 
-            //add code here...
+            for (T object : someObjects)
+                result.add(objectToJsonFunction.applyAsJson(object));
 
             return result;
         }
     }
 
     @Test
-    public void successCallFunctionalInterface() {
+    public void successCallFunctionalInterface() throws JsonProcessingException {
         ListConverter<SomeObject> ListConverter = new ListConverter<>();
 
         ObjectToJsonFunction<SomeObject> objectToJsonFunction = someObject -> {
-            //add code here...
-            return null;
+            return JSONUtils.toJSON(someObject);
         };
 
         List<String> strings = ListConverter.toJsonsList(
@@ -57,6 +60,50 @@ public class FunctionalInterfaceTest {
 
         Assertions.assertTrue(strings.contains("{\"objectName\":\"Object-1\"}"));
         Assertions.assertTrue(strings.contains("{\"objectName\":\"Object-2\"}"));
+    }
+
+    @FunctionalInterface
+    interface CurrencyConvertFunction<T> {
+        T applyConversion(T t);
+    }
+
+    static class AccountsBalanceConverter<T> {
+        public List<T> toAnotherCurrencyList(@NonNull List<T> accounts, CurrencyConvertFunction<T> currencyConvertFunction) {
+            List<T> result = new ArrayList<>();
+            if (accounts.isEmpty())
+                throw new IllegalArgumentException("The list is empty");
+
+            for (T accountBalance : accounts)
+                result.add(currencyConvertFunction.applyConversion(accountBalance));
+
+            return result;
+        }
+    }
+
+
+    @Test
+    public void successConversion(){
+        AccountsBalanceConverter<BigDecimal> accountsBalanceConverter = new AccountsBalanceConverter<>();
+
+        CurrencyConvertFunction<BigDecimal> rublesToUSD = rubles -> {
+            return rubles.divide(BigDecimal.valueOf(76.05), MathContext.DECIMAL128).stripTrailingZeros();
+        };
+
+        CurrencyConvertFunction<BigDecimal> USDToRubles = USD -> {
+            return USD.multiply(BigDecimal.valueOf(76.05), MathContext.DECIMAL128).stripTrailingZeros();
+        };
+
+        List<BigDecimal> accountsInRubles = Arrays.asList(
+                BigDecimal.valueOf(2147890.90),
+                BigDecimal.valueOf(3493290.32)
+        );
+
+        List<BigDecimal> accountsInUSD = accountsBalanceConverter.toAnotherCurrencyList(accountsInRubles, rublesToUSD);
+
+        Assertions.assertEquals(
+                accountsInRubles,
+                accountsBalanceConverter.toAnotherCurrencyList(accountsInUSD, USDToRubles)
+        );
     }
 
 }
