8cc59fb440c2eda03a9114683d5af094ad4a9fbe
Mikhail Lazukov <misha130200@gmail.com>
2022-02-10 13:14:46 +0700

    Дополнена реализация для прохождения исходных тестов и созданы собственные примеры использования стримов.

--- src/test/java/com/sbrf/reboot/streams/StreamTest.java
+++ src/test/java/com/sbrf/reboot/streams/StreamTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -21,7 +23,7 @@ public class StreamTest {
 
         List<Integer> expectedIntegers = Arrays.asList(3, 6, 8, 9);
 
-        List<Integer> actualIntegers = null; //add code here
+        List<Integer> actualIntegers = integers.stream().sorted().collect(Collectors.toList()); //add code here
 
         assertEquals(expectedIntegers, actualIntegers);
     }
@@ -36,7 +38,7 @@ public class StreamTest {
 
         List<Integer> expectedIntegers = Arrays.asList(6, 8);
 
-        List<Integer> actualIntegers = null; //add code here
+        List<Integer> actualIntegers = integers.stream().filter(i -> i % 2 == 0).collect(Collectors.toList()); //add code here
 
         assertEquals(expectedIntegers, actualIntegers);
 
@@ -71,7 +73,10 @@ public class StreamTest {
 
         );
 
-        List<Book> actualBooks = null; //add code here
+        List<Book> actualBooks = books.stream()
+                .filter(book -> book.author == "Maria")
+                .sorted(Comparator.comparing(book -> book.price))
+                .collect(Collectors.toList()); //add code here
 
         assertEquals(expectedBooks, actualBooks);
 
@@ -88,10 +93,56 @@ public class StreamTest {
 
         List<String> expectedContracts = Arrays.asList("M-NCC-1-CH", "M-NCC-2-US", "M-NCC-3-NH");
 
-        List<String> actualContracts = null; //add code here
+        List<String> actualContracts = contracts.stream().map(s -> "M-" + s).collect(Collectors.toList()); //add code here
 
         assertEquals(expectedContracts, actualContracts);
 
     }
 
+
+    @Test
+    public void multiplyFirstTwoElements() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4);
+
+        int expectedResult = 2;
+
+        int actualResult = numbers.stream().limit(2).reduce((s1, s2) -> s1 * s2).get();
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+
+    @Test
+    public void sumOfLastTwoElements() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4);
+
+        int expectedSum = 7;
+
+        int actualSum = numbers.stream().skip(numbers.size()-2).reduce((s1, s2) -> s1 + s2).get();
+
+        assertEquals(expectedSum, actualSum);
+    }
+
+    @Test
+    public void maxElementOfListStream() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4);
+
+        int expectedMaxNumber = 4;
+
+        Integer actualMaxNumber = numbers.stream().max(Integer::compareTo).get();
+
+        assertEquals(expectedMaxNumber, actualMaxNumber);
+    }
+
+
+    @Test
+    public void minElementOfListStream() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4);
+
+        int expectedMinNumber = 1;
+
+        Integer actualMinNumber = numbers.stream().min(Integer::compareTo).get();
+
+        assertEquals(expectedMinNumber, actualMinNumber);
+    }
 }
