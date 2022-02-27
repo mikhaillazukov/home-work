6263ab8e2a7a6d683d540b8c5e9ead33dcb96aeb
Mikhail Lazukov <misha130200@gmail.com>
2022-02-27 18:58:11 +0700

    Исправлена реализация TaskExecutorService для успешного прохождения тестов. Добавлена возможность изменять чисто потоков и написан тест для нее

--- src/main/java/com/sbrf/reboot/service/concurrency/TaskExecutorService.java
+++ src/main/java/com/sbrf/reboot/service/concurrency/TaskExecutorService.java
@@ -2,10 +2,11 @@ package com.sbrf.reboot.service.concurrency;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class TaskExecutorService {
 
-    private final int numberOfThreads;
+    private int numberOfThreads;
 
     private final ExecutorService service;
 
@@ -14,12 +15,19 @@ public class TaskExecutorService {
         this.numberOfThreads = numberOfThreads;
     }
 
+    public void setNumberOfThreads(int numberOfThreads) {
+        ThreadPoolExecutor executor = (ThreadPoolExecutor)this.service;
+        executor.setCorePoolSize(Math.max(numberOfThreads, 0));
+        executor.setMaximumPoolSize(Math.max(numberOfThreads, 1));
+        this.numberOfThreads = numberOfThreads;
+    }
+
     public void execute(Task task) {
-        service.execute(task);
+        for (int i = 0; i < numberOfThreads; i++)
+            service.execute(task);
     }
 
     public void shutdown() {
         service.shutdown();
     }
-
 }
